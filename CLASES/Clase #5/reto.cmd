c3e0c5cd869db18947c2da4f633ab3524469b66d
josevinicio04 <josevinicio04@gmail.com>
2024-03-08 22:15:52 -0600

    clase #5

--- CLASES/Clase #5/reto.cmd
+++ CLASES/Clase #5/reto.cmd
@@ -1,3 +1,15 @@
 #desarrollar una aplicacion con c++ de registro de personas
 con sqlite grafico.
 
+Capitulo #7
+
+#escribir un algoritmo que calcule y escriba una tabla con los 100 primeros números primos. Un número es primo si sólo
+#tiene por divisores la unidad y el propio número
+
+#escribir un programaa que genere el triangulo de pascal
+1
+1 1
+1 2 1
+1 3 3 1
+1 4 6 4 1
+1 5 10 10 5 1
